fc82f2a3924262c83750d8a66aa2bbd3d28d290b
Tushar Indurjeeth <tusharindurjeeth@protonmail.com>
2021-11-08 22:58:01 +0200

    Instagram clone

---
+++ components/Header.js
@@ -0,0 +1,105 @@
+import Image from 'next/image';
+import {
+  HeartIcon,
+  MenuIcon,
+  PaperAirplaneIcon,
+  PlusCircleIcon,
+  SearchIcon,
+  UserGroupIcon,
+} from '@heroicons/react/outline';
+import { HomeIcon } from '@heroicons/react/solid';
+import { signIn, signOut, useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import { useRecoilState } from 'recoil';
+import { modalState } from '../atoms/modalAtom';
+
+const Header = () => {
+  const { data: session } = useSession(); // rename data to session
+  const [open, setOpen] = useRecoilState(modalState);
+  const router = useRouter();
+
+  // console.log(session);
+
+  return (
+    <div className="shadow-sm border-b bg-white sticky top-0 z-50">
+      <div className="flex justify-between bg-white max-w-6xl mx-5 lg:mx-auto">
+        {/* Left */}
+        <div
+          onClick={() => router.push('/')}
+          className="relative hidden lg:inline-grid w-24 cursor-pointer"
+        >
+          <Image
+            src="https://links.papareact.com/ocw"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+
+        <div
+          onClick={() => router.push('/')}
+          className="relative w-10 lg:hidden flex-shrink-0 cursor-pointer"
+        >
+          <Image
+            src="https://links.papareact.com/jjm"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+
+        {/* Middle - Search input*/}
+        <div className="max-w-xs">
+          <div className="relative mt-1 p-3 rounded-md">
+            <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+              <SearchIcon className="h-5 w-5 text-gray-500" />
+            </div>
+
+            <input
+              className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 rounded-md 
+              focus:ring-black focus:border-black"
+              type="text"
+              placeholder="Search"
+            />
+          </div>
+        </div>
+
+        {/* Right */}
+        <div className="flex items-center justify-end space-x-4">
+          <HomeIcon onClick={() => router.push('/')} className="navBtn" />
+          <MenuIcon className="h-6 md:hidden cursor-pointer" />
+
+          {session ? (
+            <>
+              <div className="relative navBtn ">
+                <PaperAirplaneIcon className="navBtn rotate-45" />
+                <div
+                  className="absolute -top-1 -right-2 w-5 h-5 text-xs bg-red-500 
+            rounded-full flex items-center justify-center animate-pulse text-white"
+                >
+                  3
+                </div>
+              </div>
+
+              <PlusCircleIcon
+                onClick={() => setOpen(true)}
+                className="navBtn"
+              />
+              <UserGroupIcon className="navBtn" />
+              <HeartIcon className="navBtn" />
+
+              <img
+                onClick={signOut}
+                src={session.user.image}
+                alt="profile pic"
+                className="h-10 w-10 rounded-full cursor-pointer"
+              />
+            </>
+          ) : (
+            <button onClick={signIn}>Sign In</button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ components/Stories.js
@@ -0,0 +1,39 @@
+import faker from 'faker';
+import { useSession } from 'next-auth/react';
+import { useEffect, useState } from 'react';
+import Story from './Story';
+
+const Stories = () => {
+  const [suggestions, setSuggestions] = useState([]);
+  const { data: session } = useSession(); // rename data to session
+
+  useEffect(() => {
+    const suggestions = [...Array(20)].map((_, i) => ({
+      ...faker.helpers.contextualCard(),
+      id: i,
+    }));
+
+    setSuggestions(suggestions);
+  }, [setSuggestions]);
+
+  return (
+    <div className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm overflow-x-scroll scrollbar-thin scrollbar-thumb-black">
+      {session && (
+        <Story
+          key={session.user.uid}
+          img={session.user.image}
+          username={session.user.username}
+        />
+      )}
+      {suggestions.map((profile) => (
+        <Story
+          key={profile.id}
+          img={profile.avatar}
+          username={profile.username}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Stories;
